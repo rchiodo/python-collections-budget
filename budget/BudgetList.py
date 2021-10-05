9cac9bd82593d000c8b2b5f25fa3b9377e4f029f
Rich Chiodo <rchiodo@users.noreply.github.com>
2021-10-05 15:14:47 -0700

    3rd module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,8 +1,9 @@
 # %%
-if 'INSIDE_KERNEL' in globals():
+if "INSIDE_KERNEL" in globals():
     import Expense
 else:
-    from .import Expense
+    from . import Expense
+import matplotlib.pyplot as plt
 
 # %%
 class BudgetList:
@@ -14,18 +15,31 @@ class BudgetList:
         self.overages = []
 
     def append(self, item):
-        if (self.sum_expenses + item < self.budget):
+        if self.sum_expenses + item < self.budget:
             self.expenses.append(item)
             self.sum_expenses += item
         else:
             self.overages.append(item)
             self.sum_overages += item
 
+    def __iter__(self):
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
+
+    def __next__(self):
+        try:
+            return self.iter_e.__next__()
+        except:
+            return self.iter_o.__next__()
 
     def __len__(self):
         return len(self.expenses) + len(self.overages)
+
+
 #%%
 def main():
+    # %%
     myBudgetList = BudgetList(1200)
     expenses = Expense.Expenses()
     expenses.read_expenses("data/spending_data.csv")
@@ -34,6 +48,20 @@ def main():
 
     print("The count of all expenses: " + str(len(myBudgetList)))
 
+    for entry in myBudgetList:
+        print(entry)
+
+    fig, ax = plt.subplots()
+    labels = ["Expenses", "Overages", "Budget"]
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    ax.bar(labels, values, color=['green', 'red', 'blue'])
+    ax.set_title('Your total expenses vs total budget')
+
+    if "INSIDE_KERNEL" not in globals():
+        plt.show()
+
+
+#%%
 if __name__ == "__main__":
     main()
 
